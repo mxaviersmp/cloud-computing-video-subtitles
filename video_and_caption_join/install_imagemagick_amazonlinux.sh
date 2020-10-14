f35a7db58e645d56f8a11db612b1c027af94eb36
flych3r <xavier_sampaio@alu.ufc.br>
2020-10-14 16:04:04 -0300

    added files for amazon linux instalation

---
+++ video_and_caption_join/install_imagemagick_amazonlinux.sh
@@ -0,0 +1,16 @@
+sudo yum makecache fast
+sudo yum install ImageMagick ImageMagick-devel
+sudo cat << EOF >> /etc/ImageMagic/policy.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE policymap [
+<!ELEMENT policymap (policy)+>
+<!ELEMENT policy (#PCDATA)>
+<!ATTLIST policy domain (delegate|coder|filter|path|resource) #IMPLIED>
+<!ATTLIST policy name CDATA #IMPLIED>
+<!ATTLIST policy rights CDATA #IMPLIED>
+<!ATTLIST policy pattern CDATA #IMPLIED>
+<!ATTLIST policy value CDATA #IMPLIED>
+]>
+<policymap>
+</policymap>
+EOF
---
+++ video_and_caption_join/install_requirements_amazonlinux.sh
@@ -0,0 +1,2 @@
+sudo yum install python3
+python3 -m pip install -r requirements.txt
