295c7008ec9537524128ea5d57d3d15f39efdf24
Matheus Xavier Sampaio <xavier_sampaio@alu.ufc.br>
2020-10-14 12:54:18 -0300

    added temp directory creation and cors

--- video_and_caption_join/main.py
+++ video_and_caption_join/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
 import json
 import os
 import multiprocessing
@@ -11,6 +12,13 @@ API_FLASK_HOST = os.environ.get('API_FLASK_HOST')
 API_FLASK_PORT = os.environ.get('API_FLASK_PORT')
 
 app = Flask( __name__ )
+CORS(app)
+
+if not os.path.exists('temp'):
+    os.mkdir('./temp')
+    os.mkdir('./temp/info')
+    os.mkdir('./temp/original')
+    os.mkdir('./temp/captioned')
 
 def downloadFileFromBucket( bucketUri, fileName ):
 
@@ -76,7 +84,7 @@ def start_job(
         json.dump(job_info, outfile)
 
     salveFileToBucket( data['captioned_video'], fileName, outputFilePath )
-    salveFileToBucket( data['job_info'], fileName, './temp/jobInfo/{}.json'.format(fileName) )
+    salveFileToBucket( data['job_info'], fileName, './temp/info/{}.json'.format(fileName) )
 
 
 if __name__ == '__main__':
